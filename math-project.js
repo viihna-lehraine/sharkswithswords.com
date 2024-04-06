d64848f76be5e1b83c217e5d1f016a7198578d31
Viihna <viihna@voidfucker.com>
2024-04-05 23:44:28 -0400

    4/5 EOD

--- math-project.js
+++ math-project.js
@@ -1,4 +1,4 @@
-// 1 - Hypotenuse
+// Hypotenuse & Area of a Right Triangle
 document.getElementById('submitButton1').onclick = function() {
 
     a = document.getElementById("aTextBox").value;
@@ -14,7 +14,7 @@ document.getElementById('submitButton1').onclick = function() {
     document.getElementById("dLabel").innerHTML = "Area: " + rtArea;
 }
 
-// 2 - Circumference of a Circle
+// Circumference & Area of a Circle
 document.getElementById('submitButton2').onclick = function() {
 
     r = document.getElementById('radiusTextBox').value
@@ -27,7 +27,75 @@ document.getElementById('submitButton2').onclick = function() {
     document.getElementById('circleArea').innerHTML = "Area: " + circArea;
 }
 
-// Volume and Surface Area of a Sphere
+// Surface Area & Volume of a Sphere
 document.getElementById('submitButton3').onclick = function() {
 
+    rSphere = document.getElementById('sphereRadiusTextBox').value;
+    rSphere = Number(rSphere);
+
+    sphereSurfaceArea = 4 * Math.PI * (Math.pow(rSphere, 2));
+    sphereVolume = (4 / 3) * Math.PI * (Math.pow(rSphere, 3));
+
+    document.getElementById('sphereSurfaceAreaLabel').innerHTML = "Surface Area: " + sphereSurfaceArea;
+    document.getElementById('sphereVolumeLabel').innerHTML = "Volume: " + sphereVolume;
+}
+
+// Finding x in a Quadratic Function
+document.getElementById('submitButton4').onclick = function() {                   
+
+    aQuadratic = document.getElementById('aQuadraticTextBox').value;
+    aQuadratic = Number(aQuadratic);
+
+    bQuadratic = document.getElementById('bQuadraticTextBox').value;
+    bQuadratic = Number(bQuadratic);
+
+    cQuadratic = document.getElementById('cQuadraticTextBox').value;
+    cQuadratic = Number(cQuadratic);
+
+    xQuadratic1 = (((-1) * bQuadratic) + Math.sqrt((Math.pow(bQuadratic, 2)) - (4 * aQuadratic * cQuadratic))) / (2 * aQuadratic);
+    xQuadratic2 = (((-1) * bQuadratic) - Math.sqrt((Math.pow(bQuadratic, 2)) - (4 * aQuadratic * cQuadratic))) / (2 * aQuadratic);
+
+    document.getElementById('xQuadraticOutput').innerHTML = "c = " + xQuadratic1 + ", " + xQuadratic2;
+}
+
+//Tip Calculator
+bill = document.getElementById('billTextBox').value;
+bill = Number(bill);
+
+tip10 = bill * (10 / 100);
+tip15 = bill * (15 / 100);
+tip20 = bill * (20 / 100);
+tip25 = bill * (25 / 100);
+
+document.getElementById('submitButton5').onclick = function() {
+
+    document.getElementById('tipOutput1').innerHTML = "Tip = " + tip10;
+    document.getElementById('tipOutput2').innerHTML = "Total Bill: " + (bill + tip10);
+}
+
+document.getElementById('submitButton6').onclick = function() {
+
+    document.getElementById('tipOutput1').innerHTML = "Tip = " + tip15;
+    document.getElementById('tipOutput2').innerHTML = "Total Bill: " + (bill + tip15);
+}
+
+document.getElementById('submitButton7').onclick = function() {
+
+    document.getElementById('tipOutput1').innerHTML = "Tip = " + tip20;
+    document.getElementById('tipOutput2').innerHTML = "Total Bill: " + (bill + tip20);
+}
+
+document.getElementById('submitButton8').onclick = function() {
+
+    document.getElementById('tipOutput1').innerHTML = "Tip = " + tip25;
+    document.getElementById('tipOutput2').innerHTML = "Total Bill: " + (bill + tip25);
+}
+
+// Dice Roller
+
+diceSides = Number(document.getElementById('diceSideNumber').value);
+
+document.getElementById('submitButton9').onclick = function() {
+
+    document.getElementById('diceResultTotal').innerHTML = "Result (Total Sides): " + diceSides;
 }
