a8907782002d6c0aee71ddffe0b7b5450fd6a563
lost-possum <viihna@sharkswithswords.com>
2024-04-28 03:42:51 -0400

    EOD, publishing to NC

--- manifesto2.js
+++ manifesto2.js
@@ -1,3 +1,6 @@
+const ids = ['img10', 'img29', 'img39'];
+const url = "https://youareanidiot.cc"; 
+
 document.getElementById('homeButton').onclick = function() {
     window.location.href = "https://sharkswithswords.com/main.html";
 };
@@ -5,3 +8,27 @@ document.getElementById('homeButton').onclick = function() {
 document.getElementById('sitemapButton').onclick = function() {
     window.location.href = "https://sharkswithswords.com/sitemap.html";
 };
+
+ids.forEach(id => {
+    document.getElementById(id).onclick = function() {
+        window.location.href = url;
+    };
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const homeButton = document.getElementById('homeButton');
+    const sitemapButton = document.getElementById('sitemapButton');
+
+    function handleMouseEnter() {
+        this.classList.add('button-animate');
+    }
+
+    function handleMouseLeave() {
+        this.classList.remove('button-animate');
+    }
+
+    [homeButton, sitemapButton].forEach(button => {
+        button.addEventListener('mouseenter', handleMouseEnter);
+        button.addEventListener('mouseleave', handleMouseLeave);
+    });
+});
