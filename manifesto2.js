ef4d0468366c1f70c8ff3e75f8ac32ff3531a4ed
lost-possum <viihna@sharkswithswords.com>
2024-04-27 20:33:25 -0400

    progress on manifesto2.html; saving progess

---
+++ manifesto2.js
@@ -0,0 +1,7 @@
+document.getElementById('homeButton').onclick = function() {
+    window.location.href = "https://sharkswithswords.com/main.html";
+};
+
+document.getElementById('sitemapButton').onclick = function() {
+    window.location.href = "https://sharkswithswords.com/sitemap.html";
+};
